fafabaebecf64ad7e294f3862d7e5b3546e1ebd6
covolato <38336636+covolato@users.noreply.github.com>
2018-04-12 22:23:01 -0300

    Update makeSwap.sh

--- makeSwap.sh
+++ makeSwap.sh
@@ -1,3 +1,20 @@
+#!/bin/sh
+######################################################
+# Shell script to create swap file, if not exist.
+# If not, create swap of the same RAM size (Default).
+#
+# if you want a different size of the RAM size,
+# especify it in bytes at command line as $1,
+# example for 4Gb (1024 * 1024 * 4 = 4194304):
+# $ makeSwap.sh 4194304
+# Tip: To translate 4Gb in bytes try "echo $((1024 * 1024 * 4))"
+#
+# This script is not full tested, Use at your own risk.
+#
+# Autor: julio@psi.com.br
+# $Date: Qui Abr 12 16:23:20 BRT 2018
+# Licence: GPL2
+######################################################
 if free | awk '/^Swap:/ {exit !$2}'; then
         SWAP=$(free -m | awk '/^Swap:/ {print $2}')
         echo "Server already has swap of $SWAP Mb"
